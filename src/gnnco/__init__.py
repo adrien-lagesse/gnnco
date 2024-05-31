61c0da515bd8b990ed76721eabc28fea92f6f0d9
Adrien Lagesse <adrien.lagesse.pro@gmail.com>
2024-05-31 15:31:01 +0200

    Some presentation changes

--- src/gnnco/__init__.py
+++ src/gnnco/__init__.py
@@ -1,11 +1,11 @@
 """
-The `gnnco` package simplifies benchmarking and training GNNs on task issued from Combinatorial Optimization (CO).
+The `gnnco` package simplifies benchmarking and training GNNs on tasks issued from Combinatorial Optimization (CO).
 It is based on PyTorch and the default models are written using the Pytorch Geometric package.
 
 Several functionalities are provided:
 - Generating CO Datasets
 - Using existing CO Dataset
-- A framework to benchmark different GNNs architecture
+- A framework to benchmark different GNNs architectures
 - Using pretrained GNNs for generating Graph Positional Encodings
 """
 
